496a5481bd91ea19c68e11d347b7f16d91efae29
Bison Hubert <bisonhubert@gmail.com>
2020-03-30 17:04:26 -0700

    Add link to footer

--- src/components/core/Footer.vue
+++ src/components/core/Footer.vue
@@ -1,12 +1,14 @@
 <template>
   <v-footer height="60" class="mt-5 justify-center text-xs-center d-block">
     <div class="mt-2">
-      v{{ appVersion }} - {{ $t('footer.MADE_WITH') }}
+      {{ $t('footer.MADE_WITH') }}
       <v-icon small class="red--text mx-0 px-0">mdi-cards-heart</v-icon>
       &nbsp;{{ $t('footer.BY') }}:
     </div>
     <div>
-      <a href="https://daniel-avellaneda.com">https://daniel-avellaneda.com</a>
+      <a href="https://bisonhubert.com" target="_blank"
+        >https://bisonhubert.com</a
+      >
     </div>
   </v-footer>
 </template>
