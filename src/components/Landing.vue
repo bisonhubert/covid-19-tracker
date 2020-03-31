dfb0f4adb11a2be10e11b4abc72da6540ccac188
Bison Hubert <bisonhubert@gmail.com>
2020-03-30 17:16:32 -0700

    Update title an description

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,9 +1,12 @@
 <template>
   <v-container fluid>
     <v-layout row wrap>
-      <Heading :title="$t('landing.TITLE')" />
-      <Description :description="$t('landing.DESCRIPTION')" />
+      <Heading title="COVID-19 Tracking" />
+      <v-flex xs12 text-xs-center mb-2
+        >A visualization of COVID-19 by state and county. Data pulled from
+        <a href="https://covidtracking.com/api/">The COVID Tracking Project</a>.
+        Updated daily.</v-flex
+      >
     </v-layout>
-    <ProjectDescription />
   </v-container>
 </template>
